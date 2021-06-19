9078d25accc293ac70f2eeac0383161f2194d49b
aga-moj-nick <agnieszka.aga.krawczyk@gmail.com>
2021-06-19 12:37:05 +0200

    dodane ćwiczenia

---
+++ Ćwiczenie 4-1.py
@@ -0,0 +1,27 @@
+﻿wybor = input ("* - mnożenie, / - dzielenie, + - dodawanie, - - odejmowanie, ** - potęgowanie, % - modulo: ")
+
+a = int (input ("Pierwsza liczba to: "))
+b = int (input ("Druga liczba to: "))
+
+
+if (wybor == "*"):
+    print (a * b)
+
+elif (wybor == "/") :
+    if (b == 0) :
+        print ("Nie dziel przez 0!!!")
+    else :
+        print (a / b)
+
+elif (wybor == "+") :
+    print (a + b)
+
+
+elif (wybor == "-") :
+    print (a - b)
+
+elif (wybor == "**") :
+    print (a ** b)
+
+elif (wybor == "%") :
+    print (a % b)
---
+++ Ćwiczenie 4-2.py
@@ -0,0 +1,7 @@
+﻿a = int (input ("Podaj liczbę: "))
+
+
+if (a < 0) :
+    print ("Liczba: ", -a)
+else :
+    print ("Liczba: ", a)
