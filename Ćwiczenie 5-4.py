9520bced07fb6b1337b9d9791185885951473d18
aga-moj-nick <agnieszka.aga.krawczyk@gmail.com>
2021-06-19 15:57:46 +0200

    dodany plik

---
+++ Ćwiczenie 5-4.py
@@ -0,0 +1,14 @@
+﻿wynik = 0
+i = 0
+
+while i < 3:
+    a = int (input ("Podaj dodatnią liczbę parzystą: "))
+    if (a > 0) and (a % 2 == 0):
+        wynik += a
+        i += 1
+
+    else:
+        print ("To nie jest liczba parzysta dodatnia. Trzeba powtórzyć!")
+    continue
+
+print ("Aktualny wynik dodawania to: ", wynik)
