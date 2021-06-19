4c3713e5d26d16ec0342a53184ed479f4c823663
aga-moj-nick <agnieszka.aga.krawczyk@gmail.com>
2021-06-19 11:52:13 +0200

    utworzenie ćwiczenia 3-1

---
+++ Ćwiczenie 3-1.py
@@ -0,0 +1,13 @@
+print ("Jak masz na imię?")
+imie = input ()
+print ("Hej ", imie.capitalize (), ",", " jak masz na nazwisko?", sep= "")
+nazwisko = input ()
+print (imie.capitalize () + " " + nazwisko.capitalize (), ",", " ile masz lat?", sep= "")
+wiek = int (input () )
+print ("Za rok będziesz mieć", (wiek + 1))
+print ("Jakie jest Twoje hobby ", imie.capitalize (), "?", sep="")
+hobby = input ()
+print (hobby.upper (), "jest super!", "Miłego dnia :)")
+
+
+
